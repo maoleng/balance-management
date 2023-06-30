844c12c57d332c2f380ef9669152a8f2d8597cf6
Bùi Hữu Lộc <feature451@gmail.com>
2023-07-01 02:17:30 +0700

    feat: ui route

--- routes/web.php
+++ routes/web.php
@@ -4,16 +4,16 @@ use App\Http\Controllers\InvestController;
 use App\Http\Controllers\TransactionController;
 use Illuminate\Support\Facades\Route;
 
-Route::group(['prefix' => 'invest'], static function () {
-    Route::get('/', [InvestController::class, 'index']);
+Route::group(['prefix' => 'invest', 'as' => 'invest.'], static function () {
+    Route::get('/', [InvestController::class, 'index'])->name('index');
 });
 
 Route::group(['prefix' => 'transaction', 'as' => 'transaction.'], function () {
-    Route::get('/', [TransactionController::class, 'index']);
+    Route::get('/', [TransactionController::class, 'index'])->name('index');
     Route::post('/', [TransactionController::class, 'store'])->name('store');
     Route::delete('/{transaction}', [TransactionController::class, 'destroy'])->name('destroy');
 });
 
 Route::get('/', function () {
     return view('index');
-});
+})->name('index');
