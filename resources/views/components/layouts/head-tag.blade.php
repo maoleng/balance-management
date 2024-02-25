16a9d5045e15d84fbcbe9ec7f60a319732f0229a
Bùi Hữu Lộc <feature451@gmail.com>
2024-02-26 01:41:04 +0700

    feat: base view new theme

---
+++ resources/views/components/layouts/head-tag.blade.php
@@ -0,0 +1,13 @@
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, viewport-fit=cover" />
+    <meta name="apple-mobile-web-app-capable" content="yes" />
+    <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
+    <meta name="theme-color" content="#000000">
+    <title>Maolance</title>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&family=Roboto:ital,wght@0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
+
+
+    @stack('page-style')
+</head>
