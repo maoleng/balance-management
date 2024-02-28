8d6f36d6776663ea85cea5c82c51c3196bb7bb09
Bùi Hữu Lộc <feature451@gmail.com>
2024-02-28 23:14:12 +0700

    fix: add renember token

--- database/migrations/2024_02_28_152936_create_users_table.php
+++ database/migrations/2024_02_28_152936_create_users_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
             $table->string('name');
             $table->string('email');
             $table->string('avatar');
+            $table->rememberToken();
             $table->dateTime('created_at');
         });
     }
