94e57bbd60bfaf035123f0694945b0803c74b191
Bùi Hữu Lộc <feature451@gmail.com>
2024-02-28 22:57:55 +0700

    fix: csrf token

--- resources/views/components/layouts/script.blade.php
+++ resources/views/components/layouts/script.blade.php
@@ -14,6 +14,7 @@
                 url: '{{ route('subscription') }}',
                 method: 'POST',
                 data: {
+                    _token: '{{ csrf_token() }}',
                     subscription: subscription.toJSON(),
                 }
             })
