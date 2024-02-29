c2638f8b782271e624ba95dd60402750ecbbf710
Bùi Hữu Lộc <feature451@gmail.com>
2024-02-29 20:01:10 +0700

    fix: bg header landing

--- resources/views/components/main-layout/app.blade.php
+++ resources/views/components/main-layout/app.blade.php
@@ -1,10 +1,11 @@
+@php ($is_landing = \Illuminate\Support\Facades\Route::is('index'))
 <div>
-    <div class="appHeader">
+    <div class="appHeader {{ $is_landing ? 'bg-primary text-light' : '' }}">
         <div class="left">
-            @if (! \Illuminate\Support\Facades\Route::is('index'))
-            <a wire:navigate href="@yield('back')" class="headerButton goBack">
-                <ion-icon name="chevron-back-outline" role="img" class="md hydrated" aria-label="chevron-back outline"></ion-icon>
-            </a>
+            @if (! $is_landing)
+                <a wire:navigate href="@yield('back')" class="headerButton goBack">
+                    <ion-icon name="chevron-back-outline" role="img" class="md hydrated" aria-label="chevron-back outline"></ion-icon>
+                </a>
             @endif
         </div>
         <div class="pageTitle">
